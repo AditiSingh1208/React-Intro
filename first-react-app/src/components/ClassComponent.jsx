c2e4aef94051a378d04c14558157798742d1837d
AditiSingh1208 <123548974+AditiSingh1208@users.noreply.github.com>
2024-06-17 09:56:20 +0000

    Hooks and states

--- first-react-app/src/components/ClassComponent.jsx
+++ first-react-app/src/components/ClassComponent.jsx
@@ -4,9 +4,7 @@ class ClassComponent extends React.Component{
 render(){
     return (
         <div className="App">
-          <header className="App-header">
           <p>This is class Components</p>
-          </header>
         </div>
       );
 }
